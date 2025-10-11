51fae4e8e532de5490e8913d0dd2c6a08e63b116
Heitor-Dairel <hdgtlindo@gmail.com>
2025-10-10 22:58:08 -0300

    correct modularization in fibonacci

--- src/backend/helpers/__init__.py
+++ src/backend/helpers/__init__.py
@@ -1,4 +1,3 @@
 from .beep_morse import beep_config, beep_play
 from .bin_text_verifi import bin_verifi_val, str_verifi_val
 from .key_crypto import generate_key
-from .scientific_notation import format_number
